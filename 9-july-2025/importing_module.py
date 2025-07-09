de9a97adba85c57c2352c6325b2af3483a1c5a02
Sania824 <sania9aimen@gmail.com>
2025-07-09 17:50:35 +0500

    Updated to return the index of the targeted value

--- 9-july-2025/importing_module.py
+++ 9-july-2025/importing_module.py
@@ -2,5 +2,5 @@ import my_module
 
 fruits = ['apple', 'banana', 'cherry']
 
-index = my_module.find_index(fruits, 'apple')
+index = my_module.find_index(fruits, 'cherry')
 print(index)
--- 9-july-2025/my_module.py
+++ 9-july-2025/my_module.py
@@ -3,7 +3,7 @@ print("Imported module successfully")
 test = "Test String"
 def find_index(to_search, target):
     """Find the index of a value in the sequence"""
-    for i, value in enumerate(to_search, start=0):
+    for i, value in enumerate(to_search):
         if value == target:
-            return 1
+            return i
     return -1
