f1b3abd0fbc25323f6c0c3546005f7bf6e6bdd40
Sania824 <sania9aimen@gmail.com>
2025-07-14 17:55:41 +0500

    10-july-2025 additional tasks

---
+++ 10-july-2025/file_demo_w.py
@@ -0,0 +1,14 @@
+# FILE OBJECTS, WRITING TO FILES
+
+# with open('test.txt', 'r') as rf:
+#     with open('test_copy.txt', 'w') as wf:
+#         for line in rf:
+#             wf.write(line)
+
+with open('jellycat style sofa.jpeg', 'rb') as rf:
+    with open('jellycat style sofa.jpeg_copy', 'wb') as wf:
+        chunk_size = 4096
+        rf_chunk = rf.read(chunk_size)
+        while len(rf_chunk) > 0:
+            wf.write(rf_chunk)
+            rf_chunk = rf.read(chunk_size)
